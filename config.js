11950e55ded97e2f251d3d80d5396fe39cacc03d
JemoKE016 <155730931+JemoKE016@users.noreply.github.com>
2024-01-05 21:06:42 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ if (fs.existsSync('config.env')) require('dotenv').config({ path: __dirname+'/co
 
 
 //═══════[Required Variables]════════\\
-global.owner = process.env.OWNER_NUMBER || '256763456891'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
+global.owner = process.env.OWNER_NUMBER || '+254798281390'  // Make SURE its Not Be Empty, Else Bot Stoped And Errors,
 global.mongodb = process.env.MONGODB_URI || "mongodb+srv://moriceug:ykneyPF1867D9903@clustermorice.sqdmjyk.mongodb.net/?retryWrites=true&w=majority"
 global.port= process.env.PORT || 5000
 global.email = 'moriceug256@gmail.com'
@@ -12,15 +12,15 @@ global.location = 'Lahore pakistan'
 global.gurl = 'https://instagram.com/alex_david/' // add your username
 global.sudo = process.env.SUDO || "256763456891"
 global.devs = '256763456891'
-global.website = 'https://github.com/Bladeh4x/BLADE-MD' //wa.me/+91000000000000
+global.website = 'https://github.com/Bladeh4x/BLADE-MD' //
 global.THUMB_IMAGE = process.env.THUMB_IMAGE || 'https://telegra.ph/file/3f3ae4aace2d012c11509.jpg'
 module.exports = {
   sessionName: process.env.SESSION_ID || "",      //Put Your Session Id Here
   author:  process.env.PACK_AUTHER ||  'RIKOCELS BOT',
   packname:  process.env.PACK_NAME || 'Made by Rikocels',
   
-  botname:   process.env.BOT_NAME === undefined ? "RIKOCELS-MD" : process.env.BOT_NAME,
-  ownername: process.env.OWNER_NAME === undefined ? 'RIKOCELS' : process.env.OWNER_NAME,  
+  botname:   process.env.BOT_NAME === undefined ? "Jemoh-MD" : process.env.BOT_NAME,
+  ownername: process.env.OWNER_NAME === undefined ? 'Jemoh' : process.env.OWNER_NAME,  
   auto_read_status :  process.env.AUTO_READ_STATUS === undefined ? false : process.env.AUTO_READ_STATUS,
   autoreaction:  process.env.AUTO_REACTION  === undefined ? true : process.env.AUTO_REACTION ,
   antibadword :  process.env.ANTI_BAD_WORD === undefined ? 'nbwoed' : process.env.ANTI_BAD_WORD,
